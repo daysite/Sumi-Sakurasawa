4237950facaf503cdc93d69e82d0b86a53035714
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2025-06-04 22:59:03 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -81,7 +81,7 @@ let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 let menu = `  
-*•/• Sumi Sakurasawa •/•*
+🍄 \`Sumi Sakurasawa\`
  
 > 👤 Creador : Sharet.xyz
 > 🌍 Pais : Perú 🇵🇪
